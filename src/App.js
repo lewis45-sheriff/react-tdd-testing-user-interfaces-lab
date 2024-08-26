6fc3e156d3624741fd02f323d1f16f5788c65137
lewis <lewiskipkemoi53@gmail.com>
2024-08-26 11:11:27 +0300

    all

--- src/App.js
+++ src/App.js
@@ -1,5 +1,16 @@
+import React from "react";
+
 function App() {
-  return <div>Your code here</div>;
+  return (
+    <div>
+      <h1>Hi, I'm (Your Name)</h1>
+      <img src="https://via.placeholder.com/350" alt="My profile pic" />
+      <h2>About Me</h2>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vivamus lacinia odio vitae vestibulum vestibulum.</p>
+      <a href="https://github.com/yourusername">GitHub</a>
+      <a href="https://linkedin.com/in/yourusername">LinkedIn</a>
+    </div>
+  );
 }
 
 export default App;
